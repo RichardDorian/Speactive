ee592b1dce44d8b6edf380137099a7f6f76b5077
RichardDorian <richard.dorian@outlook.com>
2023-10-16 22:40:40 +0200

    refactor: :recycle: switch to bun
    Made changes to the configuration files, updated the libraries in order to work with Bun.

---
+++ lib/dom.d.ts
@@ -0,0 +1,4 @@
+// Adding bun-types causes TypeScript to ignore compilerOptions.lib
+// This is a temporary workaround until bun-types is fixed
+// https://github.com/oven-sh/bun/issues/5134
+/// <reference lib="DOM" />
